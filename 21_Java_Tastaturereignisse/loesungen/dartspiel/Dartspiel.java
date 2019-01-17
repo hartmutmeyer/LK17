61c7360859fc21afb92107737268ebc1e144be2d
Hartmut Meyer <h.meyer6@schule.bremen.de>
2019-01-17 13:55:16 +0100

    :

--- 21_Java_Tastaturereignisse/loesungen/dartspiel/Dartspiel.java
+++ 21_Java_Tastaturereignisse/loesungen/dartspiel/Dartspiel.java
@@ -14,58 +14,67 @@ public class Dartspiel extends HJFrame implements KeyListener {
 	private static final int HEIGHT = 500;
 	private static final Color BACKGROUND = Color.WHITE;
 	private static final Color FOREGROUND = Color.BLACK;
-	private Pfeil pfeil;
+	private Pfeil p;
 
 	public Dartspiel(final String title) {
 		super(WIDTH, HEIGHT, BACKGROUND, FOREGROUND, title);
-		addKeyListener (this);
-		pfeil = new Pfeil (100,100); 
-		Timer timer = new Timer (20, this);
-		timer.start();	
+		// eigene Initialisierung
+		p = new Pfeil(50, 50);
+		Timer timer = new Timer(10, this);
+		timer.start();
+		addKeyListener(this);
 	}
-	
+
 	private void dartscheibe(Graphics g) {
 		g.drawOval (400,200,30,30);
 		g.drawOval (385,185,60,60);
 		g.drawOval(370, 170, 90, 90);
 		g.drawOval(355, 155, 120,120);
 	}
-
+	
 	@Override
 	public void myPaint(Graphics g) {
+		// wird aufgerufen, wenn das Fenster neu gezeichnet wird
 		dartscheibe(g);
-		pfeil.zeichnen(g);
-		pfeil.bewegen();
+		p.zeichnen(g);
+		p.bewegen();
 	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_N:
-			pfeil.fallenLassen();
-			break;
-		case KeyEvent.VK_S:
-			pfeil.schiessen();
-			break;
-		}
-		
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {}
-
-	@Override
-	public void keyTyped(KeyEvent e) {}
 
 	public static void main(final String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Dartspiel anwendung = new Dartspiel("Dart-Spiel");
+					Dartspiel anwendung = new Dartspiel("Dartspiel");
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+	//	System.out.println(e.getKeyCode());
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		switch (e.getKeyChar()) {
+		case 'n':
+		case 'N':
+			p.fallenLassen();
+			break;
+		case 's':
+		case 'S':
+			p.schiessen();
+			break;
+		case 'r':
+		case 'R':
+			p.reset();
+		}
+		System.out.println("Taste wurde gedrückt: " + e.getKeyChar());
 	}
 }
--- 21_Java_Tastaturereignisse/loesungen/dartspiel/Pfeil.java
+++ 21_Java_Tastaturereignisse/loesungen/dartspiel/Pfeil.java
@@ -3,12 +3,16 @@ import java.awt.Graphics;
 public class Pfeil {
 	private int x;
 	private int y;
+	private int xStart;
+	private int yStart;
 	private int speedX;
 	private int speedY;
 
 	public Pfeil(int x, int y) {
 		this.x = x;
 		this.y = y;
+		xStart = x;
+		yStart = y;
 	}
 
 	public void zeichnen(Graphics g) {
@@ -29,6 +33,13 @@ public class Pfeil {
 	
 	public void schiessen() {
 		speedX = 5;
+		speedY = 0;
+	}
+	
+	public void reset() {
+		x = xStart;
+		y = yStart;
+		speedX = 0;
 		speedY = 0;
 	}
 }
