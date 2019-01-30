30c373eb31760bd1576746be91ebc7ccfd3d039e
Hartmut Meyer <h.meyer6@schule.bremen.de>
2019-01-30 09:23:44 +0100

    .

--- 24_Java_GUI-Komponenten/WelcherButton.java
+++ 24_Java_GUI-Komponenten/WelcherButton.java
@@ -6,8 +6,8 @@ import javax.swing.border.EmptyBorder;
 public class WelcherButton extends JFrame implements ActionListener {
 	private static final int WIDTH = 300;
 	private static final int HEIGHT = 120;
-	JButton eins, zwei, drei;
-	JLabel text, text2;
+	JButton btnEins, btnZwei, btnDrei;
+	JLabel lblAusgabe1, lblAusgabe2;
 
 	public WelcherButton(final String title) {
 		super(title);
@@ -17,38 +17,39 @@ public class WelcherButton extends JFrame implements ActionListener {
 		contentPane.setLayout(new FlowLayout());
 		setContentPane(contentPane);
 		setSize(WIDTH, HEIGHT);
-		eins = new JButton("1");
-		contentPane.add(eins);
-		eins.addActionListener(this);
-		zwei = new JButton("2");
-		contentPane.add(zwei);
-		zwei.addActionListener(this);
-		drei = new JButton("3");
-		contentPane.add(drei);
-		drei.addActionListener(this);
-		text = new JLabel("Es wurde Button _ gedrückt.");
-		contentPane.add(text);
-		text2 = new JLabel("Es wurde Button _ gedrückt.");
-		contentPane.add(text2);
+		btnEins = new JButton("1");
+		contentPane.add(btnEins);
+		btnEins.addActionListener(this);
+		btnZwei = new JButton("2");
+		contentPane.add(btnZwei);
+		btnZwei.addActionListener(this);
+		btnDrei = new JButton("3");
+		contentPane.add(btnDrei);
+		btnDrei.addActionListener(this);
+		lblAusgabe1 = new JLabel("Es wurde Button _ gedrückt.");
+		contentPane.add(lblAusgabe1);
+		lblAusgabe2 = new JLabel("Es wurde Button _ gedrückt.");
+		contentPane.add(lblAusgabe2);
 		setVisible(true);
 	}
 
+	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton) e.getSource();
 
 		// 1. Alternative: lange Variante
-		if (button == eins) {
-			text.setText("Es wurde Button 1 gedrückt.");
+		if (button.equals(btnEins)) {
+			lblAusgabe1.setText("Es wurde Button 1 gedrückt.");
 		}
-		if (button == zwei) {
-			text.setText("Es wurde Button 2 gedrückt.");
+		if (button.equals(btnZwei)) {
+			lblAusgabe1.setText("Es wurde Button 2 gedrückt.");
 		}
-		if (button == drei) {
-			text.setText("Es wurde Button 3 gedrückt.");
+		if (button.equals(btnDrei)) {
+			lblAusgabe1.setText("Es wurde Button 3 gedrückt.");
 		}
 
 		// 2. Alternative: kurze Variante
-		text2.setText("Es wurde Button " + button.getText() + " gedrückt.");
+		lblAusgabe2.setText("Es wurde Button " + button.getText() + " gedrückt.");
 	}
 
 	public static void main(final String[] args) {
